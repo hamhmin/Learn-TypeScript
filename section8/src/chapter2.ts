d7a5040c7a4ce275bf2068488984d7323fb6c4c5
hhm <hamhmin1031@gmail.com>
2025-06-04 20:42:21 +0900

    push

--- section8/src/chapter2.ts
+++ section8/src/chapter2.ts
@@ -6,10 +6,10 @@ interface Person {
   name: string;
   age: number;
 }
-
-// function getPropertyKey(person:Person, key:"name"|"age") {
-// function getPropertyKey(person: Person, key: keyof Person) {
-function getPropertyKey(person: Person, key: keyof typeof personTypeOf) {
+function getPropertyKey1(person: Person, key: "name" | "age") {}
+function getPropertyKey2(person: Person, key: keyof Person) {}
+// keyof Person은 "name"|"age" 로 변경된다, 문자열 리터럴 타입의 유니온 타입으로 변경됨
+function getPropertyKey3(person: Person, key: keyof typeof personTypeOf) {
   return person[key];
 }
 
@@ -18,7 +18,7 @@ const person: Person = {
   age: 26,
 };
 
-getPropertyKey(person, "name"); // 함형민
+getPropertyKey3(person, "name"); // 함형민
 
 // typeof를 타입 정의시 사용하면 typeof 뒤에 오는 곳에서 타입을 뽑아온다.
 type PersonTypeOf = typeof personTypeOf;
